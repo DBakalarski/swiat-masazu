ad95fd7271dfbf96a9498fcbae2376f3f8371911
Dawid Bakalarski <dawid.bakalarski@gmail.com>
2020-03-11 20:33:04 +0100

    Header works 20%

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,11 +2,40 @@ import React from 'react';
 import styles from './Header.module.scss'
 
 const Header = () => {
-    const { header } = styles
+    const { header, header_menu, logo, nav, nav_item, description, main_description, separator, additional_description } = styles
     return (
         <section className={header}>
             <div className="container">
-                Sekcja Header
+                <div className={header_menu}>
+
+                    <div className={logo}>
+                        Logo
+                    </div>
+                    <div className={nav}>
+                        <div className={nav_item}>
+                            <img src="" alt="" />
+                            <p>Rezerwacja</p>
+                        </div>
+                        <div className={nav_item}>
+                            <img src="" alt="" />
+                            <p>Lokalizacja</p>
+                        </div>
+                        <div className={nav_item}>
+                            <img src="" alt="" />
+                            <p>Menu</p>
+                        </div>
+                    </div>
+                </div>
+                <div className={description}>
+                    <div className={main_description}>
+                        <h1>Masaż jest najlepszą formą relaksu dla ciała i duszy</h1>
+                        <span className={separator}></span>
+                    </div>
+                    <div className={additional_description}>
+                        <h2>Specjalizuję się w masażach klasycznych i leczniczych.</h2>
+                    </div>
+                </div>
+
             </div>
         </section>
     )
