d58d6656a42afb8b8576b30de01d17cfeda345df
Dawid Bakalarski <dawid.bakalarski@gmail.com>
2020-03-16 18:36:48 +0100

    Add office section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import './App.scss';
 import Header from './Components/Header/Header'
 import Pricing from './Components/Pricing/Pricing'
+import OfficeOffer from './Components/OfficeOffer/OfficeOffer'
 import Nav from './Components/Nav/Nav'
 
 class App extends Component {
@@ -12,6 +13,7 @@ class App extends Component {
       <React.Fragment>
         <Header />
         <Pricing />
+        <OfficeOffer />
         {/* <Nav /> */}
       </React.Fragment>
     )
---
+++ src/Components/OfficeOffer/OfficeOffer.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import styles from './OfficeOffer.module.scss'
+
+const OfficeOffer = () => {
+    const { office, title, separator, description, email, main_description, button, additional_description, office_container, book_container, circle, circle_top, circle_time, circle_minutes, cricle_button, title_container } = styles
+    return (
+        <section className={office}>
+            <div className="container">
+                <div className={office_container}>
+                    <div className={title_container}>
+                        <div className={title}>
+                            Oferta dla firm
+                    <span className={separator}></span>
+                        </div>
+                        <div className={description}>
+                            <p className={main_description}>Work<span>Site</span></p>
+                            <p className={additional_description}>Masaż biurowy</p>
+                        </div>
+                        <button className={button}>Uzyskaj indywidulaną ofertą</button>
+                        <div className={email}>klausiaswiatmasaz@gmail.com</div>
+                    </div>
+                    <div className={book_container}>
+                        <div className={circle}>
+                            <div className={circle_time}>15</div>
+                            <div className={circle_minutes}>minut</div>
+                            <button className={cricle_button}>Zamawiam</button>
+                        </div>
+                        <div className={circle_top}>
+                            <div className={circle_time}>20</div>
+                            <div className={circle_minutes}>minut</div>
+                            <button className={cricle_button}>Zamawiam</button>
+                        </div>
+
+                        <div className={circle}>
+                            <div className={circle_time}>30</div>
+                            <div className={circle_minutes}>minut</div>
+                            <button className={cricle_button}>Zamawiam</button>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </section>
+    )
+}
+
+export default OfficeOffer;
