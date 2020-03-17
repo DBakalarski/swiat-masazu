09151ce324bd8be4b50a6f8cb303d61ae44f829b
Dawid Bakalarski <dawid.bakalarski@gmail.com>
2020-03-17 19:08:50 +0100

    Add pregnant section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.scss';
 import Header from './Components/Header/Header'
 import Pricing from './Components/Pricing/Pricing'
 import OfficeOffer from './Components/OfficeOffer/OfficeOffer'
+import PregnantOffer from './Components/PregnantOffer/PregnantOffer'
 import Nav from './Components/Nav/Nav'
 
 class App extends Component {
@@ -14,6 +15,7 @@ class App extends Component {
         <Header />
         <Pricing />
         <OfficeOffer />
+        <PregnantOffer />
         {/* <Nav /> */}
       </React.Fragment>
     )
---
+++ src/Components/PregnantOffer/PregnantOffer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './PregnantOffer.module.scss'
+
+const PregnantOffer = () => {
+    const { pregnant } = styles
+    return (
+        <div className={pregnant}>Pregnant Offer</div>
+    );
+}
+
+export default PregnantOffer;
