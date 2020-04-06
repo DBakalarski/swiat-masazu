fa134eedaaf8999c432b70d7d03f285ff76022c7
Dawid Bakalarski <dawid.bakalarski@gmail.com>
2020-04-06 21:27:55 +0200

    Updates

---
+++ precache-manifest.d631615ca06483db8175cd80468c2518.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "521db0489688a60de648374fe52a7386",
+    "url": "/swiat-masazu/index.html"
+  },
+  {
+    "revision": "a5662a4cd8263f16bc2f",
+    "url": "/swiat-masazu/static/css/2.7c3fc33d.chunk.css"
+  },
+  {
+    "revision": "575555a715aab9bd13fc",
+    "url": "/swiat-masazu/static/css/main.d00de44a.chunk.css"
+  },
+  {
+    "revision": "a5662a4cd8263f16bc2f",
+    "url": "/swiat-masazu/static/js/2.914145c7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/swiat-masazu/static/js/2.914145c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "575555a715aab9bd13fc",
+    "url": "/swiat-masazu/static/js/main.0e6a41ce.chunk.js"
+  },
+  {
+    "revision": "2b6af110fca5057b253d",
+    "url": "/swiat-masazu/static/js/runtime-main.6a123364.js"
+  },
+  {
+    "revision": "8508362973bbe274f36c85c733b1e896",
+    "url": "/swiat-masazu/static/media/arrow.85083629.svg"
+  },
+  {
+    "revision": "6c300e28adf98f3154e3db23c0fd6942",
+    "url": "/swiat-masazu/static/media/check.6c300e28.svg"
+  },
+  {
+    "revision": "aef7a1f76327b9ace74bf34f4dcd2db0",
+    "url": "/swiat-masazu/static/media/close.aef7a1f7.svg"
+  },
+  {
+    "revision": "f659f79f66b57aa72010822dd7aa8d68",
+    "url": "/swiat-masazu/static/media/facebook.f659f79f.svg"
+  },
+  {
+    "revision": "1cf66d3cba087197deb9fc8b9c78e681",
+    "url": "/swiat-masazu/static/media/hamburger_menu.1cf66d3c.svg"
+  },
+  {
+    "revision": "1d4ca00d19df194784fe0b82d71757e6",
+    "url": "/swiat-masazu/static/media/klaudia_footerS.1d4ca00d.png"
+  },
+  {
+    "revision": "6b61784bacd99d065b0cc9d018f26b69",
+    "url": "/swiat-masazu/static/media/logo.6b61784b.svg"
+  },
+  {
+    "revision": "6d724f1168c55a1dec9a70cdd78f6661",
+    "url": "/swiat-masazu/static/media/mail.6d724f11.svg"
+  },
+  {
+    "revision": "d8aaa98a3a076e42ddfecd4bab0da492",
+    "url": "/swiat-masazu/static/media/masaz-plecy.d8aaa98a.svg"
+  },
+  {
+    "revision": "22caf4d6c3aaa1cb94063e163a9ec2f7",
+    "url": "/swiat-masazu/static/media/masaz-plecy_2.22caf4d6.svg"
+  },
+  {
+    "revision": "08286e190e0b5afea07e94b9540dab8c",
+    "url": "/swiat-masazu/static/media/masaz-stopy.08286e19.svg"
+  },
+  {
+    "revision": "9433e2279c450184f7d51d769ed7e27f",
+    "url": "/swiat-masazu/static/media/masaz-zuchwa.9433e227.svg"
+  },
+  {
+    "revision": "29e852ed45a5a0cd1ad01a1344fb854b",
+    "url": "/swiat-masazu/static/media/massage.29e852ed.png"
+  },
+  {
+    "revision": "ed7e47568f0c6cf76af397db7593f446",
+    "url": "/swiat-masazu/static/media/phone.ed7e4756.svg"
+  },
+  {
+    "revision": "0234f8ba47be559769f2ecefa25a77f1",
+    "url": "/swiat-masazu/static/media/pin-blue.0234f8ba.svg"
+  },
+  {
+    "revision": "2b8cd53ff4a561f8460ea721074ccac7",
+    "url": "/swiat-masazu/static/media/pin.2b8cd53f.svg"
+  },
+  {
+    "revision": "434ac53b207e636299c9369256115fb6",
+    "url": "/swiat-masazu/static/media/pin2.434ac53b.svg"
+  },
+  {
+    "revision": "620c06a87a003a4477fb22ebfbfe71d5",
+    "url": "/swiat-masazu/static/media/pregnant.620c06a8.png"
+  },
+  {
+    "revision": "904813f7bf96a8c63dbd78c763874de9",
+    "url": "/swiat-masazu/static/media/sign-blue.904813f7.svg"
+  },
+  {
+    "revision": "af6e14b851ee7fff84b9740f3d3dea0e",
+    "url": "/swiat-masazu/static/media/sign.af6e14b8.svg"
+  }
+]);
